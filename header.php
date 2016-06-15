ddfbe812852f7f48e898bcd7f3525f1ad65d9e1c
carlyn <carlyns@gmail.com>
2016-06-15 09:28:58 -0500

    initial

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+
+  <link rel="shortcut icon" href="coop_favicon.ico" type="image/ico" width="16" height="16">
+
+	<meta name="keywords" content="housing, cooperative, living, chicago, affordable, home, apartment, condo, ownership, property, roommates">
+	<meta name="description" content="Empowering/bringing together the community of housing cooperatives in Chicago.">
+
+	<link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+
+<!-- THIS STYLESHEET IS LOADED FOR ALL PAGES -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/coop.css">
+<!-- FONT/ICON GLYPHS LOADED FOR ALL PAGES
+	<link rel="stylesheet" href="css/font.css">-->
+
+	<!-- THIS STYLESHEET MIGHT CHANGE BETWEEN PAGES.
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/coop.css"> -->
+
+	<?php wp_head(); ?> <!-- THIS TAG POINTS WP TO THIS FILE TO PULL THE HEADER WHEN IT'S CALLED IN OTHER FILES.  THAT'S WHY IT NEEDS TO BE IN THIS FILE. -->
---
+++ js/coop.js
@@ -0,0 +1,55 @@
+var main = function() {
+
+
+// TOGGLING THE MENU ICON -- off for now
+/*
+$(".icon-menu3").click(function() {
+    $(".menu").toggle();
+  });
+}
+*/
+
+
+
+
+// TRYING TO SLIDE THE MENU IN FROM THE RIGHT on click
+$(".icon-menu3").click(function() {
+    $(".menu").animate({right: "-30px"});
+    $(".icon-rocket").toggle({duration: 500});
+  });
+
+// TRYING TO SLIDE THE MENU IN FROM THE RIGHT on click
+$(".icon-rocket").click(function() {
+    $(".menu").animate({right: "-350px"});
+    $(".icon-rocket").toggle({duration: 500});
+  });
+
+
+
+}
+
+// SMOOTH SCROLLING TO ANCHORS ON PAGE
+// snippet from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+// HIDE TOPBAR (INLUDING NAV) AFTER TWO SECONDS, ONLY ON MOBILE???
+
+// ALSO HIDE IT (INLUDING NAV) ON CLICK OF NAV.
+
+
+
+$(document).ready(main);
